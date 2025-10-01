0afd358d064723dfd99af2c316f0191fb13da06e
meriem-feriel-tahari <taharimeriemferiel@gmail.com>
2025-10-01 13:08:10 +1300

    one to many

--- store/models.py
+++ store/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Collection(models.Model):
+    title=models.CharField(max_length=255)
+    
 class product(models.Model):
     title=models.CharField(max_length=250)
     description=models.TextField()
     price=models.DecimalField(decimal_places=2,max_digits=6)#this two arguments are required
     inventory=models.IntegerField()
     last_update=models.DateTimeField(auto_now=True)
+    collection=models.ForeignKey(Collection,)
+    # chaque collection has multiple proucts
+    # so it is like one to many relationship one collection has many products 
     
 class Customer(models.Model):
     # this sku is going to be created in case i want the sku filed to be the id 
@@ -36,4 +42,13 @@ class order(models.Model):
         (Failed,"Failed"),
              ],
     payment_status=models.CharField(max_length=1,choices=Choices,default=Pending)
+    # customer=models.ForeignKey(Customer,on_delete=models.CASCADE,primary_key=True)  
+
+class adress(models.Model):
+    street=models.CharField(max_length=20)    
+    city=models.CharField(max_length=20)  
+    customer=models.OneToOneField(Customer,on_delete=models.CASCADE,primary_key=True)  
+    # this on delet arguments tell that if the customer is deleted all its orders are deleted 
+    # and the primary key tells that this is going to be considered as the primary key
+    
     
